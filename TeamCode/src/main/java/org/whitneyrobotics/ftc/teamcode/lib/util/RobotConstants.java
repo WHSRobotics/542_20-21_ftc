f4d4ef61c5256c62915f1d89e491c960c209de7a
Varun Bharadwaj <epicvarun@outlook.com>
2020-09-26 16:46:47 -0700

    Fixes PIDF, RobotConstants, and adds in PIDF Test

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/lib/util/RobotConstants.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/lib/util/RobotConstants.java
@@ -5,7 +5,7 @@ package org.whitneyrobotics.ftc.teamcode.lib.util;
 import org.whitneyrobotics.ftc.teamcode.lib.control.ControlConstants;
 
 public class RobotConstants {
-
+    //Drivetrain
     public static double DEADBAND_DRIVE_TO_TARGET = 24.5;
     public static double DEADBAND_ROTATE_TO_TARGET = 1.0;
     public static double drive_min = .2;//.1245;
@@ -16,20 +16,9 @@ public class RobotConstants {
     public static ControlConstants DRIVE_CONSTANTS = new ControlConstants(1.7, 0.7, 0.8);
     public static ControlConstants ROTATE_CONSTANTS = new ControlConstants(1.1, 0.07542, 0.15);
 
-    public static double R_KP = 1.10;//1.19;
-    public static double R_KI = 0.07542;
-    public static double R_KD = 0.15;
-    public static double D_KP = 0;//1.7;
-    public static double D_KI = 0;//.7
-    public static double D_KD = 0;//.8
-    public static double A_KP = 0.0;
-    public static double A_KI = 0.0;
-    public static double A_KD = 0.0;
-    public static double A_KF = 0.0;
-    public static double E_KP = 0.006;
-    public static double E_KI = 0.00000542;
-    public static double E_KD = 0.0001;
-
     public static double rotateTestAngle = 180;
     public static boolean rotateOrientation = true;
+
+    //Outtake
+    public static double OUTTAKE_MAX_VELOCITY = 1800;
 }
