a940a15e9e015b3f62e7539b09c83c972cfa150f
Aditya Mukerjee <madityaalexmukerjee@gmail.com>
2020-09-28 16:37:02 -0700

    Worked on Intake and Flywheel

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/subsys/Flywheel.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/subsys/Flywheel.java
@@ -1,4 +1,34 @@
 package org.whitneyrobotics.ftc.teamcode.subsys;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.whitneyrobotics.ftc.teamcode.lib.util.Toggler;
+
 public class Flywheel {
+    DcMotorEx launcher;
+    public final double FLYWHEEL_POWER = 0.5;
+    public Flywheel(HardwareMap flyMap){
+        launcher = flyMap.get(DcMotorEx.class, "FlyWheel");
+    }
+    Toggler flyWheelTog = new Toggler(2);
+    public int launchState;
+    public void operate(boolean togInc, boolean togDec){
+        flyWheelTog.changeState(togInc, togDec);
+        launchState = flyWheelTog.currentState();
+        switch (launchState){
+            case 0:
+                launcher.setPower(0);
+                break;
+            case 1:
+                launcher.setPower(FLYWHEEL_POWER);
+                break;
+        }
+    }
+    public void On (){
+        launcher.setPower(0);
+    }
+    public  void Off (){
+        launcher.setPower(FLYWHEEL_POWER);
+    }
 }
--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/subsys/Intake.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/subsys/Intake.java
@@ -11,20 +11,31 @@ public class Intake {
 
     private Toggler wheelToggler = new Toggler(2);
 
+    public int intakeState;
+
     public final double INTAKE_POWER = 0.45;
 
     public Intake(HardwareMap intakeWheel) {
         wheelIntake = intakeWheel.get(DcMotorEx.class, "Wheel Intake");
     }
-
-    public void operateIntakeWheel(boolean gamepadInput1) {
-        wheelToggler.changeState(gamepadInput1);
-        if (wheelToggler.currentState()==0) {
-            wheelIntake.setPower(0);
+    public void operateIntakeWheel(boolean gamepadInput) {
+        wheelToggler.changeState(gamepadInput);
+        intakeState = wheelToggler.currentState();
+        switch (intakeState){
+            case 0:
+                wheelIntake.setPower(0);
+                break;
+            case 1:
+                wheelIntake.setPower(INTAKE_POWER);
+                break;
         }
 
-        else {
-            wheelIntake.setPower(INTAKE_POWER);
-        }
+    }
+    // for use in Auto
+    public void On (){
+        wheelIntake.setPower(INTAKE_POWER);
+    }
+    public void Off(){
+        wheelIntake.setPower(0);
     }
 }
