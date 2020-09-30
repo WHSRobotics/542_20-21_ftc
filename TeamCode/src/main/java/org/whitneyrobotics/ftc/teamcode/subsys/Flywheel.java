6894fc61c493d5f5c2c4ae8bc059b3566bad44f0
Aditya Mukerjee <madityaalexmukerjee@gmail.com>
2020-09-30 16:35:19 -0700

    Added field constants to flywheel for aiming

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/subsys/Flywheel.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/subsys/Flywheel.java
@@ -6,6 +6,9 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.whitneyrobotics.ftc.teamcode.lib.util.Toggler;
 
 public class Flywheel {
+    public final double POWER_SHOT_TARGET_HEIGHT = 584.2;
+    public final double MID_GOAL_HEIGHT = 685.8;
+    public final double HIGH_TARGET_HEIGHT = 901.7;
     DcMotorEx launcher;
     public final double FLYWHEEL_POWER = 0.5;
     public Flywheel(HardwareMap flyMap){
@@ -13,6 +16,7 @@ public class Flywheel {
     }
     Toggler flyWheelTog = new Toggler(2);
     public int launchState;
+    //public void calculateLaunchHeading ()
     public void operate(boolean togInc, boolean togDec){
         flyWheelTog.changeState(togInc, togDec);
         launchState = flyWheelTog.currentState();
