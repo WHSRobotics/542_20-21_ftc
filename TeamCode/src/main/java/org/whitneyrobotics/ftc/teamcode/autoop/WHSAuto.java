ca1ee3e9084fab06ec42aca270c898ea53d3c9d8
Kavita Pandya <kavpan8@gmail.com>
2020-10-10 15:51:56 -0700

    define states, advance states, and enable states for auto

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/autoop/WHSAuto.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/autoop/WHSAuto.java
@@ -1,6 +1,7 @@
 package org.whitneyrobotics.ftc.teamcode.autoop;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.vuforia.INIT_ERRORCODE;
 
 import org.whitneyrobotics.ftc.teamcode.lib.geometry.Coordinate;
 import org.whitneyrobotics.ftc.teamcode.lib.geometry.Position;
@@ -45,6 +46,7 @@ public class WHSAuto extends OpMode {
     SwerveToTarget driveToStartingPointFromWobbleOne;
     SwerveToTarget driveToStartingPointFromWobbleTwo;
     SwerveToTarget driveToStartingPointFromWobbleThree;
+
     private void instantiateSwerveToTargets() {
         Position[] startToFoundationSwervePositions = {startingCoordinateArray[STARTING_ALLIANCE], foundationStartingPositionArray[STARTING_ALLIANCE]};
         Position[] foundationToWallSwervePositions = {foundationStartingPositionArray[STARTING_ALLIANCE], foundationMovedPositionArray[STARTING_ALLIANCE]};
@@ -55,8 +57,43 @@ public class WHSAuto extends OpMode {
         Position[] foundationMidpointToFoundationMidpointTwoPositions = {foundationMovedPositionArray[STARTING_ALLIANCE], pullFoundationMidpointArray[STARTING_ALLIANCE]};
         Position[] startToParkingSwervePositions = {pullFoundationMidpointArray[STARTING_ALLIANCE], parkingPositions[STARTING_ALLIANCE]};
     }
+    //State definitions
+    static final int INIT = 0;
+    static final int DRIVE_TO_LAUNCH_POINT = 1;
+    static final int LAUNCH_PARTICLES_AND_SCAN_STACK = 2;
+    static final int DROP_OFF_WOBBLE_GOAL = 3;
+    static final int PARK_ON_LAUNCH_LINE = 4;
+    static final int END = 5;
+
+    static final int NUMBER_OF_STATES = 6;
+
+    boolean[] stateEnabled = new boolean[NUMBER_OF_STATES];
+
+    int state = INIT;
+    int subState = 0;
+
+    public void advanceState() {
+        if (stateEnabled[(state + 1)]) {
+            state++;
+            subState = 0;
+        } else {
+            state++;
+            advanceState();
+        }
+
+    public void defineStateEnabledStatus(){
+            stateEnabled[INIT] = true;
+            stateEnabled[DRIVE_TO_LAUNCH_POINT] = true;
+            stateEnabled[LAUNCH_PARTICLES_AND_SCAN_STACK] = true;
+            stateEnabled[DROP_OFF_WOBBLE_GOAL] = true;
+            stateEnabled[PARK_ON_LAUNCH_LINE] = true;
+            stateEnabled[END] = true;
+
+        }
+
     @Override
     public void init() {
+
 
     }
 
--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/teleop/WHSTeleOp.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/teleop/WHSTeleOp.java
@@ -39,46 +39,7 @@ public class WHSTeleOp extends OpMode {
         }else{
             robot.drivetrain.operateMecanumDriveScaled(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x, robot.getCoordinate().getHeading());
         }
-        //Outtake Launcher
-        if (gamepad2.x){
-            robot.outtake.operateTargetLaunch(gamepad2.x, gamepad2.y);
-        }
-        else {
-            robot.outtake.operateTargetLaunch(false, false);
-        }
-        //Outtake Angle
-        if (gamepad2.left_bumper){
-            robot.outtake.operateLaunchAngle(gamepad2.left_bumper, gamepad2.right_bumper);
-        }
-        else if (gamepad2.right_bumper){
-            robot.outtake.operateLaunchAngle(gamepad2.left_bumper, gamepad2.right_bumper);
-        }
-
-        //Wobble Claw
-        if (gamepad2.a) {
-            robot.wobble.operateClaw(gamepad2.a);
-        }
-        else {
-            robot.wobble.operateClaw(false);
-        }
-        //Wobble Arm
-        if (gamepad2.dpad_down) {
-            robot.wobble.operateArm(gamepad2.dpad_down); //inside the robot at start of the match
-        }
-        else if (gamepad2.dpad_up) {
-            robot.wobble.operateArm(gamepad2.dpad_up); //raise arm above the boundry
-        }
-        else if (gamepad2.dpad_right) {
-            robot.wobble.operateArm(gamepad2.dpad_right); //raise arm up
-        }
-        else if (gamepad2.dpad_left) {
-            robot.wobble.operateArm(gamepad2.dpad_left); //raise arm down
-        }
-        telemetry.addData("Outtake Motor State",robot.outtake.launchStateDescription);
-        telemetry.addData("Outtake Angle State", robot.outtake.angleStateDescription);
-        telemetry.addData("Canister State", robot.canister.canisterState);
-        telemetry.addData("Intake State", robot.intake.intakeStateDescription);
-        telemetry.addData("Wobble Arm State", robot.wobble.armStateDescription);
-        telemetry.addData("Wobble Claw State", robot.wobble.clawStateDescription);
+        //Outtake
+        if (gamepad2.)
     }
 }
