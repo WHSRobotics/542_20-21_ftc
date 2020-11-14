abca4633969af0d11ce20de91b4a18121b60c26a
Varun Bharadwaj <epicvarun@outlook.com>
2020-11-13 18:45:16 -0800

    Continues making Updates Strafe To Target

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/lib/purepursuit/strafetotarget/StrafeFollower.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/lib/purepursuit/strafetotarget/StrafeFollower.java
@@ -1,11 +1,16 @@
 package org.whitneyrobotics.ftc.teamcode.lib.purepursuit.strafetotarget;
 
+import org.whitneyrobotics.ftc.teamcode.lib.control.PIDController;
 import org.whitneyrobotics.ftc.teamcode.lib.geometry.Coordinate;
 import org.whitneyrobotics.ftc.teamcode.lib.geometry.Position;
+import org.whitneyrobotics.ftc.teamcode.lib.geometry.StrafeWaypoint;
+import org.whitneyrobotics.ftc.teamcode.lib.geometry.SwerveWaypoint;
 import org.whitneyrobotics.ftc.teamcode.lib.motion.RateLimiter;
 import org.whitneyrobotics.ftc.teamcode.lib.purepursuit.PurePursuitRobotConstants;
 import org.whitneyrobotics.ftc.teamcode.lib.util.Functions;
 import org.whitneyrobotics.ftc.teamcode.subsys.Drivetrain;
+
+import java.util.ArrayList;
 
 public class StrafeFollower {
 
@@ -28,7 +33,7 @@ public class StrafeFollower {
     private double kV = PurePursuitRobotConstants.STRAFE_KV;
     private double kA = PurePursuitRobotConstants.STRAFE_KV;
 
-    private double trackWidth = Drivetrain.getTrackWidth();
+        private double trackWidth = Drivetrain.getTrackWidth();
     private double wheelBase = Drivetrain.getWheelBase();
 
     private boolean inProgress;
@@ -43,8 +48,8 @@ public class StrafeFollower {
         double[] currentWheelVelocities = {currentBackVelocities[1] - (frontRightVelocity - currentBackVelocities[0]), frontRightVelocity, currentBackVelocities[0], currentBackVelocities[1]};
 
         boolean tFound = false;
-        for (int i = lastIndex; i < smoothedPath.length - 1; i++) {
-            Double nextTValue = new Double(calculateT(smoothedPath[i], smoothedPath[i + 1], lookaheadDistance));
+        for (int i = lastIndex; i < path.size() - 1; i++) {
+            Double nextTValue = calculateT(path.getCoordinateAtIndex(i), path.getCoordinateAtIndex(i + 1), path.followerConstants.getLookaheadDistance());
 
             if (!tFound && !nextTValue.isNaN() && (nextTValue + i) > (currentTValue + lastIndex)) {
                 tFound = true;
@@ -53,22 +58,21 @@ public class StrafeFollower {
             }
         }
 
-        Position calculatedTStartPoint = smoothedPath[lastIndex];
-        Position calculatedTEndPoint = smoothedPath[lastIndex + 1];
-        lookaheadPoint = Functions.Positions.add(calculatedTStartPoint, Functions.Positions.scale(currentTValue, Functions.Positions.subtract(calculatedTEndPoint, calculatedTStartPoint)));
+        Position calculatedTStartPoint = path.getCoordinateAtIndex(lastIndex);
+        Position calculatedTEndPoint = path.getCoordinateAtIndex(lastIndex+1);
+        Position lookaheadPoint = Functions.Positions.add(calculatedTStartPoint, Functions.Positions.scale(currentTValue, Functions.Positions.subtract(calculatedTEndPoint, calculatedTStartPoint)));
 
-        int indexOfClosestPoint = calculateIndexOfClosestPoint(smoothedPath);
-        int indexOfClosestHeading = calculateIndexOfClosestHeading();
+        int indexOfClosestPoint = calculateIndexOfClosestPoint(path.getWaypoints(), currentCoord);
+        int indexOfClosestHeading = calculateIndexOfClosestHeading(currentCoord);
 
         Position vectorToLookaheadPoint = Functions.Positions.subtract(lookaheadPoint, currentCoord);
         vectorToLookaheadPoint = Functions.field2body(vectorToLookaheadPoint, currentCoord);
         double angleToLookaheadPoint = Math.toDegrees(Math.atan2(vectorToLookaheadPoint.getY(), vectorToLookaheadPoint.getX()));
-        angleToLookaheadPointDebug = angleToLookaheadPoint;
 
         headingController.calculate(targetAngularVelocities[indexOfClosestHeading] - angularVelocity);
         double headingFeedback = headingController.getOutput();
 
-        currentTargetWheelVelocities = calculateTargetWheelVelocities(targetVelocities[indexOfClosestPoint], angleToLookaheadPoint, targetAngularVelocities[indexOfClosestHeading]);
+        currentTargetWheelVelocities = calculateTargetWheelVelocities(path.getTangentialVelocityAtIndex(indexOfClosestPoint), angleToLookaheadPoint, path.getAngularVelocityAtIndex(indexOfClosestHeading));
 
         double deltaTime = System.nanoTime() / 1E9 - lastTime;
         double[] targetWheelAccelerations = new double[4];
@@ -95,4 +99,113 @@ public class StrafeFollower {
         return new double[] {0.0, 0.0, 0.0, 0.0};
     }
 
+
+    private double calculateT(Position lineStart, Position lineEnd, double lookaheadDistance) {
+        // constants used throughout the method
+        Position d = Functions.Positions.subtract(lineEnd, lineStart);
+        Position f = Functions.Positions.subtract(lineStart, currentCoord);
+        double r = lookaheadDistance;
+
+        double a = Functions.Positions.dot(d, d);
+        double b = 2 * Functions.Positions.dot(f, d);
+        double c = Functions.Positions.dot(f, f) - r * r;
+
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant < 0) {
+            // no intersection
+        } else {
+            // ray didn't totally miss sphere, so there is a solution to the equation.
+            discriminant = Math.sqrt(discriminant);
+
+            // either solution may be on or off the ray so need to test both
+            // t1 is always the smaller value, because BOTH discriminant and a are nonnegative.
+            double t1 = (-b - discriminant) / (2 * a);
+            double t2 = (-b + discriminant) / (2 * a);
+
+            // 3x HIT cases:
+            //          -o->             --|-->  |            |  --|->
+            // Impale(t1 hit,t2 hit), Poke(t1 hit,t2>1), ExitWound(t1<0, t2 hit),
+
+            // 3x MISS cases:
+            //       ->  o                     o ->              | -> |
+            // FallShort (t1>1,t2>1), Past (t1<0,t2<0), CompletelyInside(t1<0, t2>1)
+
+            if (t1 >= 0 && t1 <= 1) {
+                // t1 is the intersection, and it's closer than t2 (since t1 uses -b - discriminant)
+                // Impale, Poke
+                return t1;
+            }
+
+            if (t2 >= 0 && t2 <= 1) {
+                return t2;
+            }
+        }
+        return Double.NaN;
+    }
+
+
+    private int calculateIndexOfClosestPoint(ArrayList<StrafeWaypoint> smoothedPath, Coordinate currentCoord) {
+        // creates array in which we store the current distance to each point in our path
+
+        double[] distances = new double[path.size()];
+
+        for (int i = 0
+        /*lastClosestPointIndex*/
+        ; i < path.size(); i++) {
+            distances[i] = Functions.Positions.subtract(path.getCoordinateAtIndex(i), currentCoord).getMagnitude();
+        }
+
+        // calculates the index of value in the array with the smallest value and returns that index
+        lastClosestPointIndex = Functions.calculateIndexOfSmallestValue(distances);
+        return lastClosestPointIndex;
+    }
+
+    public double[] calculateTargetTranslationalWheelVelocities(double targetVelocity, double angleToLookaheadPoint) {
+        double targetVelocityX = targetVelocity * Functions.cosd(angleToLookaheadPoint);
+        double targetVelocityY = targetVelocity * Functions.sind(angleToLookaheadPoint);
+        double k = (trackWidth + wheelBase) / 2;
+
+        double vFL = targetVelocityX - targetVelocityY;// - k * angleToLookaheadPoint;
+        double vFR = targetVelocityX + targetVelocityY;// + k * angleToLookaheadPoint;
+        double vBL = targetVelocityX + targetVelocityY;// - k * angleToLookaheadPoint;
+        double vBR = targetVelocityX - targetVelocityY;// + k * angleToLookaheadPoint;
+
+        return new double[]{vFL, vFR, vBL, vBR};
+    }
+
+    public double[] calculateTargetWheelVelocities(double targetVelocity, double angleToLookaheadPoint, double targetAngularVelocity) {
+        targetAngularVelocity = Math.toRadians(targetAngularVelocity);
+        double targetVelocityX = targetVelocity * Functions.cosd(angleToLookaheadPoint);
+        double targetVelocityY = targetVelocity * Functions.sind(angleToLookaheadPoint);
+        double k = (trackWidth + wheelBase) / 2;
+
+        double vFL = targetVelocityX - targetVelocityY - k * targetAngularVelocity;
+        double vFR = targetVelocityX + targetVelocityY + k * targetAngularVelocity;
+        double vBL = targetVelocityX + targetVelocityY - k * targetAngularVelocity;
+        double vBR = targetVelocityX - targetVelocityY + k * targetAngularVelocity;
+
+        return new double[]{vFL, vFR, vBL, vBR};
+    }
+
+    private int calculateIndexOfClosestHeading(Coordinate currentCoord) {
+        boolean closestHeadingFound = false;
+        double currentHeading = currentCoord.getHeading();
+        double[] headingDiffs = new double[path.size()];
+        for (int i = lastClosestHeadingIndex; i < path.size()-1; i++) {
+            if (!closestHeadingFound && Math.abs(currentHeading - path.getCoordinateAtIndex(i+1).getHeading()) < Math.abs(currentHeading - path.getCoordinateAtIndex(lastClosestHeadingIndex).getHeading())) {
+                if (i < path.size() - 2) {
+                    if (Math.abs(path.getCoordinateAtIndex(i+2).getHeading()- currentHeading) >= Math.abs(path.getCoordinateAtIndex(i+1).getHeading() - currentHeading)) {
+                        conditionMet = true;
+                        lastClosestHeadingIndex = i + 1;
+                        closestHeadingFound = true;
+                    }
+                } else {
+                    closestHeadingFound = true;
+                }
+            }
+            //headingDiffs[i] = Math.abs(currentHeading - smoothedPath[i].getHeading());
+        }
+        //lastClosestHeadingIndex = Functions.calculateIndexOfSmallestValue(headingDiffs);
+        return lastClosestHeadingIndex;
+    }
 }
