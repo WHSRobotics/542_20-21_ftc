066354b0e2ae67ea3c2fb7e684fa0c6e7a8653bc
Aditya Mukerjee <madityaalexmukerjee@gmail.com>
2020-12-14 16:35:29 -0800

    Reorganized auto Swerve stuff

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/autoop/AutoSwervePositions.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/autoop/AutoSwervePositions.java
@@ -6,16 +6,6 @@ import java.util.ArrayList;
 
 public class AutoSwervePositions {
 
-    // swerve Lookahed Distances
-    public static double startToShotlineLookaheadDist = 350; // in mm
-    // swerve spacing
-    public static double startToShotlineWaypointSpacing = 80; // in mm
-    // swerve weight smooth
-    public static double startToShotlineWeightSmooth = 0.5;
-    // swerve turn speed
-    public static double startToShotlineTurnSpeed = 750;
-    // swerve max velocity
-    public static double startToShotlineMaxVelocity = 3; // 1 - 5
     // declare positions here put spaces between each path you are generating for and label which path it is for
     // test path
     public static Position p1Test = new Position(1,2);
@@ -24,6 +14,21 @@ public class AutoSwervePositions {
     public static Position p1StartToShotline = new Position(-1500, -600);
     public static Position p2StartToShotLine = new Position(-600, 0);
     public static Position p3StartToShotLine = new Position(0, -300);
+
+    // swerve Lookahed Distances
+    public static double startToShotlineLookaheadDist = 350; // in mm
+
+    // swerve spacing
+    public static double startToShotlineWaypointSpacing = 80; // in mm
+
+    // swerve weight smooth
+    public static double startToShotlineWeightSmooth = 0.5;
+
+    // swerve turn speed
+    public static double startToShotlineTurnSpeed = 750;
+
+    // swerve max velocity
+    public static double startToShotlineMaxVelocity = 3; // 1 - 5
 
     //make  ArrayLists here Call these in getPath
     public static ArrayList<Position> testPath = new ArrayList<Position>();
