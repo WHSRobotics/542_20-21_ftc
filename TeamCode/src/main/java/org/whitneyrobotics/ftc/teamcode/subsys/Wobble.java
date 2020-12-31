193679f3dbf7bcbb79ec64b11321bd211a615861
Abhi Prajapati <abhiketan1@gmail.com>
2020-12-30 16:58:49 -0800

    Wobble Changes to fit new Wobble Subsystem

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/subsys/Wobble.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/subsys/Wobble.java
@@ -4,9 +4,48 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.whitneyrobotics.ftc.teamcode.lib.control.PIDFController;
 import org.whitneyrobotics.ftc.teamcode.lib.util.Toggler;
 
 public class Wobble {
+    private Servo claw;
+    private Servo trapDoor;
+    private DcMotor linearSlide;
+
+    private Toggler clawToggler = new Toggler(2);
+
+    public Wobble(HardwareMap wobbleMap) {
+        claw = wobbleMap.servo.get("clawServo");
+        linearSlide = (DcMotor) wobbleMap.dcMotor.get("armMotor");
+    }
+
+    public enum ClawPositions {
+        OPEN, CLOSE
+    }
+
+    public int[] ARM_POSITIONS = {0, 1, 2, 3}; //placeholder, these are motor tick counts set to represent vertical position on the linear slide
+
+    public double[] CLAW_POSITIONS = {0, 90}; // rest, push
+
+    public String clawStateDescription;
+    public void operateClaw(boolean gamepadInput) {
+        clawToggler.changeState(gamepadInput);
+        if (clawToggler.currentState() == 0) {
+            claw.setPosition(CLAW_POSITIONS[OldWobble.ClawPositions.OPEN.ordinal()]);
+            clawStateDescription = "Open";
+        } else {
+            claw.setPosition(CLAW_POSITIONS[OldWobble.ClawPositions.CLOSE.ordinal()]);
+            clawStateDescription = "Close";
+        }
+    }
+
+    public void setClawPosition(Wobble.ClawPositions clawPosition){
+        claw.setPosition(CLAW_POSITIONS[clawPosition.ordinal()]);
+    }
+
+    public void setLinearSlidePosition(Wobble.ARM_POSITIONS linearSlidePosition){
+        linearSlidePosition.setPosition(ARM_POSITIONS[linearSlidePosition()]);
+    }
+
+
 
 }
