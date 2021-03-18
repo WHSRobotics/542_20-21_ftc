734d349329687b3ce9c548640e75e1677658c250
Aditya Mukerjee <adityaalexmukerjee@gmail.com>
2021-03-17 23:21:22 -0700

    Created ShootingTest file

---
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/tests/ShootingTest.java
@@ -0,0 +1,17 @@
+package org.whitneyrobotics.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Shooting Test", group = "Tests")
+public class ShootingTest extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
